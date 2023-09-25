115531174579fe64ebfc9bba49f64437b4d0b775
MMDREZA <mheidari831003.game@gmail.com>
2023-09-25 16:40:41 +0330

    lottory

---
+++ login_flutter-main/lib/ui/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'login.dart';
+
+class Home extends StatelessWidget {
+  Home({super.key});
+
+  final Box _boxLogin = Hive.box("login");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("صفحه اصلی"),
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(0.5),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white),
+              ),
+              child: IconButton(
+                onPressed: () {
+                  _boxLogin.clear();
+                  _boxLogin.put("loginStatus", false);
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return const Login();
+                      },
+                    ),
+                  );
+                },
+                icon: const Icon(Icons.logout_rounded),
+              ),
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(130, 10, 120, 20),
+            child: Column(
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: const Text(
+                    "خوش آمدید 🎉",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  _boxLogin.get("userName"),
+                  style: const TextStyle(
+                    fontSize: 22,
+                    color: Colors.black54,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () {},
+              child: const Text(
+                'صفحه قرعه کشی',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ login_flutter-main/lib/ui/login.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'home.dart';
+import 'signup.dart';
+
+class Login extends StatefulWidget {
+  const Login({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  final FocusNode _focusNodePassword = FocusNode();
+  final TextEditingController _controllerUsername = TextEditingController();
+  final TextEditingController _controllerPassword = TextEditingController();
+
+  bool _obscurePassword = true;
+  final Box _boxLogin = Hive.box("login");
+  final Box _boxAccounts = Hive.box("accounts");
+
+  @override
+  Widget build(BuildContext context) {
+    if (_boxLogin.get("loginStatus") ?? false) {
+      return Home();
+    }
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 150),
+              Text(
+                "خوش آمدید",
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                "ورود به برنامه",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              const SizedBox(height: 60),
+              TextFormField(
+                controller: _controllerUsername,
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                  labelText: "نام کاربری",
+                  prefixIcon: const Icon(Icons.person_outline),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onEditingComplete: () => _focusNodePassword.requestFocus(),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "لطفا نام کاربری را به صوزت انگلیسی وارد نمایید";
+                  } else if (!_boxAccounts.containsKey(value)) {
+                    return "نام کاربری را اشتباه وارد کرده اید";
+                  }
+
+                  return null;
+                },
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _controllerPassword,
+                focusNode: _focusNodePassword,
+                obscureText: _obscurePassword,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  labelText: "رمز عبور",
+                  prefixIcon: const Icon(Icons.password_outlined),
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _obscurePassword = !_obscurePassword;
+                        });
+                      },
+                      icon: _obscurePassword
+                          ? const Icon(Icons.visibility_outlined)
+                          : const Icon(Icons.visibility_off_outlined)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "لطفا رمز عبور را وارد نمایید";
+                  } else if (value !=
+                      _boxAccounts.get(_controllerUsername.text)) {
+                    return "رمز عبور اشتباه است";
+                  }
+
+                  return null;
+                },
+              ),
+              const SizedBox(height: 60),
+              Column(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        _boxLogin.put("loginStatus", true);
+                        _boxLogin.put("userName", _controllerUsername.text);
+
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return Home();
+                            },
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text("ورود به برنامه"),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          _formKey.currentState?.reset();
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const Signup();
+                              },
+                            ),
+                          );
+                        },
+                        child: const Text("ثبت نام"),
+                      ),
+                      const Text("آیا ثبت نام نکرده اید؟"),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _focusNodePassword.dispose();
+    _controllerUsername.dispose();
+    _controllerPassword.dispose();
+    super.dispose();
+  }
+}
